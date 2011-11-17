93a845eba64edfbca80f31df9ad84ae2710c70ff
Levent Ali <lebreeze@gmail.com>
2011-11-17 19:26:03 +0000

    Initial Commit

---
+++ PachStat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PachStat
+//
+//  Created by Levent Ali on 17/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ PachStat/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  PachStat
+//
+//  Created by Levent Ali on 17/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
